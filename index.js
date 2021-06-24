86e8aad1cae0c006c37c8bfcc7d4831fd4b7df5b
max-allen <contact@maxallen.io>
2021-06-23 21:04:45 -0400

    adds podcast ingest

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
-const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+
 const router = express.Router()
 const axios = require('axios')
 const db = require('./db')
+const { Podcast, Episode, UserPodcast } = require('./db/models')
 
 router.post('/', async (req, res, next) => {
-  const { url } = req.body
+  const { url, userId } = req.body
   try {
     const resp = await axios({ method: 'get', url })
-    res.json({ ...resp.data })
-  } catch(err) {
+
+    let podcast = await Podcast.findOne({ where: { title: resp.data.title } })
+
+    if (podcast) {
+      await UserPodcast.create({ userId, podcastId: podcast.id })
+      res.json({ podcast })
+    } else {
+      const {
+        title: podcastTitle,
+        description: podcastDescription,
+        icon: iconUrl,
+        feed_url: feedUrl,
+        items: podcastEpisodes
+      } = resp.data
+
+      podcast = await Podcast.create({ title: podcastTitle, description: podcastDescription, iconUrl, feedUrl })
+
+      await UserPodcast.create({ userId, podcastId: podcast.id })
+
+      const episodeObjects = podcastEpisodes.map(({
+        title,
+        content_text: description,
+        image: imageUrl,
+        attachments
+      }) => {
+        const [audioData] = attachments
+        const { url: audioUrl } = audioData
+
+        return ({
+          title,
+          description,
+          imageUrl,
+          audioUrl,
+          podcastId: podcast.id
+        })
+      })
+
+      const episodes = await Episode.bulkCreate(episodeObjects)
+
+      const rawPodcastObj = podcast.get({ plain: true })
+      rawPodcastObj.episodes = episodes
+
+      res.json({ podcast: rawPodcastObj })
+    }
+  } catch (err) {
     next(err)
   }
 })
@@ -22,7 +66,7 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors())
-app.use('/api', router)
+app.use('/api/podcasts', router)
 
 db.sync()
 
